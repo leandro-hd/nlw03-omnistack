a69b74b340a0f89ba6a76d4077714369a43184b8
leandro-hd <leandrohg2003@gmail.com>
2020-10-16 17:03:44 -0300

    Comitando...

---
+++ server/src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import path from 'path';
+
+import routes from './routes';
+import errorHandler from './errors/handler';
+
+import './database/connection'
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errorHandler);
+
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.listen(3333);
